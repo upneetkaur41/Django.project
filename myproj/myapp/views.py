c8450eb9d73256ef22359c807cfe7a07bf2de27f
upneetkaur41 <123930263+upneetkaur41@users.noreply.github.com>
2023-03-06 18:54:04 +0530

    Add files via upload

---
+++ myproj/myapp/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from myapp.models import 
+
+# def addition(num1,num2):
+#     res = int(num1)+int(num2)
+#     return res
+# def subtract(num1,num2):
+#     res = int(num1)-int(num2)
+#     return res
+# def multiply(num1,num2):
+#     res = int(num1)*int(num2)
+#     return res
+# def divide(num1,num2):
+#     res = int(num1)/int(num2)
+#     return res
+
+def demo(request):
+    # num1 = request.GET.get('num1','')
+    # num2 = request.GET.get('num2','')
+    # form = f"""
+    # <div id = "output">
+    #     <form method = "GET"  >
+    #         <input type = "text" id="num1" name = "num1" value="{num1}" />
+    #         <input type = "text" id="num2" name = "num2" value="{num2}"/>
+    #         <input type = "submit" name="add" value="+" />
+    #         <input type = "submit" name="sub" value="-" />
+    #         <input type = "submit" name="mul" value="*" />
+    #         <input type = "submit" name="div" value="/" /> 
+    #     </form>
+    # </div>
+     
+    #  """
+    # result = " "
+    
+    # if  request.method  == "GET":
+        
+    #     print(request.GET)
+    #     if 'add' in request.GET:
+    #         result = addition(num1,num2)
+    #     if 'sub' in request.GET:
+    #         result = subtract(num1,num2)
+    #     if 'mul' in request.GET:
+    #         result = multiply(num1,num2)
+    #     if 'div' in request.GET:
+    #         result = divide(num1,num2)
+       
+    # return HttpResponse(f"<html><body>{form}<h1>Output:{result}</h1></body></html>")
+
+    form = BlogForm()
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            blog = form.save()
+            blog.is_published = True
+            blog.save()
+    return render(request,'index.html', {'form': form})
